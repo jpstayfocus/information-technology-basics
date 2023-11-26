cbdaf89e05198e26bfc23e61c16ee51887c74821
Jephte Pierre <110998062+jpstayfocus@users.noreply.github.com>
2023-11-25 21:19:19 -0500

    Create array.js

---
+++ Javascript/array.js
@@ -0,0 +1,35 @@
+// array is simply an ordered list of values
+// arrays are zero-indexed
+
+// create an array
+
+var friends = ['Mike', 'Anna', 'Jenny'] // index 0: Mike / index 1: Anna / index 3: Jenny
+
+// accessing element in a array
+
+friends[0] // Mike
+
+// getting array size
+
+friends.length // 3, number element
+
+// find the last element in the array
+firends[friends.length-1]
+
+// adding element to an array
+friends[3] = "Georges"
+
+friends
+friends = ['Mike', 'Anna', 'Jenny', 'Georges']
+
+fiends[friends.length] = "Maria"
+
+friends
+friends = ['Mike', 'Anna', 'Jenny', 'Georges', 'Maria']
+
+// removing element from the end of an array
+
+friends.pop()
+
+friends
+friends = ['Mike', 'Anna', 'Jenny', 'Georges']
